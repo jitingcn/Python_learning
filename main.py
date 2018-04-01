07c5fa4deaf082c54fa5b91a3026095a55f19ba7
jiting <jitingchina@gmail.com>
2018-04-01 05:46:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python_learning - main.py
+# Created by JT on 4/1/2018 05:37.
+# Blog: https://blog.jtcat.com/
+# 
+__author__ = 'JT <jiting@jtcat.com>'
+
+print("Hello world!")
