dbb577c288bea7027b66ae862038a7a84abbb2a6
jiting <jitingchina@gmail.com>
2018-04-05 08:13:46 +0100

    Update: rewrite decode_QQ_whoSaid.py as OOP style "0.3 Beta"

--- exercises/others/decode_QQ_whoSaid.py
+++ exercises/others/decode_QQ_whoSaid.py
@@ -6,51 +6,53 @@
 # Blog: https://blog.jtcat.com/
 
 # reference: https://www.zhaoj.in/read-4821.html
+# Put captured json data into data.json in the directory first!
 __author__ = 'JT <jiting@jtcat.com>'
-__version__ = "0.2 Alpha"
+__version__ = "0.3 Beta"
 
 import json
 
-data = """
 
-"""
-data = json.loads(data)
+class Main(object):
 
+    def __init__(self):
+        self.keys = {"oe": "0", "n": "0", "z": "0", "on": "0",
+                     "oK": "1", "6": "1", "5": "1",
+                     "ow": "2", "-": "2", "A": "2", "oc": "2",
+                     "oi": "3", "i": "3", "o": "3", "oz": "3",
+                     "7e": "4", "v": "4", "P": "4", "7n": "4",
+                     "7K": "5", "4": "5", "k": "5", "7": "5", "7v": "5",
+                     "7w": "6", "C": "6", "s": "6", "7c": "6",
+                     "7i": "7", "S": "7", "l": "7", "7z": "7",
+                     "Ne": "8", "c": "8", "F": "8", "Nn": "8", "ov": "8",
+                     "NK": "9", "E": "9", "q": "9", "Nv": "9",
+                     }
+        with open("data.json", "rb") as f:
+            raw = json.load(f)
+        self.data = raw["data"]["confesses"]
 
-def get_user(text):
-    keys = {"oe": "0", "n": "0", "z": "0", "on": "0",
-            "oK": "1", "6": "1", "5": "1",
-            "ow": "2", "-": "2", "A": "2", "oc": "2",
-            "oi": "3", "i": "3", "o": "3", "oz": "3",
-            "7e": "4", "v": "4", "P": "4", "7n": "4",
-            "7K": "5", "4": "5", "k": "5", "7": "5", "7v": "5",
-            "7w": "6", "C": "6", "s": "6", "7c": "6",
-            "7i": "7", "S": "7", "l": "7", "7z": "7",
-            "Ne": "8", "c": "8", "F": "8", "Nn": "8", "ov": "8",
-            "NK": "9", "E": "9", "q": "9", "Nv": "9",
-            }
+    def decode(self, data):
+        result = ""
+        string = data.replace("*S1*", "")
 
-    # text = ""
-    final_string = ""
-
-    text = text.replace("*S1*", "")
-
-    while text:
-        if len(text) > 1:
-            if not keys.__contains__(text[:2]):
-                current_value = keys.get(text[:1])
-                text = text[1:]
+        while string:
+            if len(string) > 1:
+                if self.keys.__contains__(string[:2]):
+                    current_value = self.keys.get(string[:2])
+                    string = string[2:]
+                else:
+                    current_value = self.keys.get(string[:1])
+                    string = string[1:]
+                result += str(current_value)
             else:
-                current_value = keys.get(text[:2])
-                text = text[2:]
-            final_string = final_string + str(current_value)
-        else:
-            final_string = final_string + str(keys.get(text))
-            text = ""
-    return final_string
+                result += str(self.keys.get(string))
+                string = ""
+        return result
+
+    def start(self):
+        for i in self.data:
+            print(self.decode(i["fromEncodeUin"]))
 
 
-for i in data["data"]["confesses"]:
-    print(get_user(i["fromEncodeUin"]))
-
-get_user("")
+if __name__ == "__main__":
+    Main().start()
