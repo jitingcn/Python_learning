0e561051b55371ffbf70c6e26ba7d8f4bf38a091
jiting <jitingchina@gmail.com>
2018-05-23 00:49:48 +0100

    Update: add date_check to 3.2_File_handling

--- exercises/school/3.2_File_handling.py
+++ exercises/school/3.2_File_handling.py
@@ -9,7 +9,7 @@ import os
 import json
 # Part 1 – Reading birthdays from a file
 
-# def getBirthdays(fileName, book):
+# Questions asked: def getBirthdays(fileName, book):
 # According to Python coding style guide (https://www.python.org/dev/peps/pep-0008/#function-and-variable-names)
 # Both function and argument name should be lowercase. Separate function name should use underline, not capital.
 
@@ -26,10 +26,39 @@ def get_birthdays(filename, book=None):
 # Part 2 – A complete birthday book application
 
 
+def date_check(book):
+    month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    long_month = ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"]
+    short_month = ["Feb", "Apr", "Jun", "Sep", "Nov"]
+    remove_set = set()
+    print("\nSimple check of dates")
+    for i in book:
+        if book[i]["month"] not in month:
+            print(i + ":", book[i]["day"], book[i]["month"])
+            print("The month of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
+            print("Prepare remove", i, "from birthday book")
+            remove_set.add(i)
+        if book[i]["month"] in long_month:
+            if not 1 < book[i]["day"] < 31:
+                print(i + ":", book[i]["day"], book[i]["month"])
+                print("The day of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
+                print("Prepare remove", i, "from birthday book")
+                remove_set.add(i)
+        if book[i]["month"] in short_month:
+            if not 1 < book[i]["day"] < 30:
+                print(i + ":", book[i]["day"], book[i]["month"])
+                print("The day of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
+                print("Prepare remove", i, "from birthday book")
+                remove_set.add(i)
+    for i in remove_set:
+        del book[i]
+    return book
+
+
 def name_birth(book, name):
     try:
         if name in book:
-            print(name + "'s birthday is", book[name]["day"], book[name]["month"])
+            print("\n" + name + "'s birthday is", book[name]["day"], book[name]["month"])
         else:
             raise IndexError
     except IndexError:
@@ -39,7 +68,7 @@ def name_birth(book, name):
 def month_birth(book, month):
     count = 0
     try:
-        print("\nChecking birthday in", month)
+        print("\nChecking birthday")
         for i in book:
             if month == book[i]["month"]:
                 print(i + ":", book[i]["day"], month)
@@ -50,8 +79,16 @@ def month_birth(book, month):
 
 
 def next_birth(book, month, day):  # TODO
-    pass
-
+    count = 0
+    try:
+        print("\nThese people's birthdays within a week after", day, month)
+        for i in book:
+            if month == book[i]["month"]:
+                print(i + ":", book[i]["day"], month)
+                count += 1
+        print(count, "people's birthday in", month)
+    except IndexError:
+        print()
 
 
 def test_data(method, book=None):  # create and delete test data
@@ -64,6 +101,7 @@ def test_data(method, book=None):  # create and delete test data
                     "Duncan,May,20\n"
                     "Holland,Jan,3\n"
                     "Stephenson,Feb,14\n"
+                    "Stephen,Jan,32\n"
                     )
     elif method == 1:  # delete test data
         os.remove("test.txt")
@@ -78,9 +116,11 @@ def main():  # TODO
     print("Birthday book by Jiting (testing)")
     test_data(0)
     data = get_birthdays("test.txt")
-    print(data)
+    data = date_check(data)
+    # print(data)
     name_birth(data, "Susan")
     month_birth(data, "Mar")
+    # next_birth(data, "Mar", 20)
     test_data(2, data)
     test_data(3)
     test_data(1)
