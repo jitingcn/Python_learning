bbf65a443439dc9fe789ad04fc5c476fea8319c5
jiting <jitingchina@gmail.com>
2018-05-23 15:33:31 +0100

    Update(3.2_File_handling): Refactoring function: next_birth

--- exercises/school/3.2_File_handling.py
+++ exercises/school/3.2_File_handling.py
@@ -30,8 +30,8 @@ def get_birthdays(filename, book=None):
 
 def date_check(book):
     month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    long_month = ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"]
-    short_month = ["Feb", "Apr", "Jun", "Sep", "Nov"]
+    month_x = {"Jan": 31, "Feb": 29, "Mar": 31, "Apr": 30, "May": 31, "Jun": 30,
+               "Jul": 31, "Aug": 31, "Sep": 30, "Oct": 31, "Nov": 30, "Dec": 31}
     remove_set = set()
     print("\nSimple check of dates")
     for i in book:
@@ -40,23 +40,16 @@ def date_check(book):
             print(i + ":", book[i]["day"], book[i]["month"])
             print("Prepare remove", i, "from birthday book")
             remove_set.add(i)
-        elif book[i]["month"] in long_month:
-            if not 1 <= book[i]["day"] <= 31:
-                print("\nThe day of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
-                print(i + ":", book[i]["day"], book[i]["month"])
-                print("Prepare remove", i, "from birthday book")
-                remove_set.add(i)
-        elif book[i]["month"] in short_month:
-            if not 1 <= book[i]["day"] <= 30:
-                print("\nThe day of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
-                print(i + ":", book[i]["day"], book[i]["month"])
-                print("Prepare remove", i, "from birthday book")
-                remove_set.add(i)
+        elif not 1 <= book[i]["day"] <= month_x[book[i]["month"]]:
+            print("\nThe day of", i + "'s birthday seen not right. Please make sure there are no misspellings.")
+            print(i + ":", book[i]["day"], book[i]["month"])
+            print("Prepare remove", i, "from birthday book")
+            remove_set.add(i)
         if book[i]["month"] == "Feb":
-            if not 1 <= book[i]["day"] <= 28:
+            if book[i]["day"] == 29:
                 print("\nThe day of", i + "'s birthday seen not right.")
                 print(i + ":", book[i]["day"], book[i]["month"])
-                print("Warning: The possibility of a birthday on February", book[i]["day"], "is extremely low!")
+                print("Warning: The possibility of a birthday on February 29th is extremely low!")
     for i in remove_set:
         del book[i]
     return book
@@ -87,11 +80,11 @@ def month_birth(book, month):
         print()
 
 
-def next_birth(book, month, day):  # TODO: boundary definition problems still exist!
+def next_birth(book, month, day):
     try:
         month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        long_month = ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"]
-        short_month = ["Feb", "Apr", "Jun", "Sep", "Nov"]
+        month_x = {"Jan": 31, "Feb": 28, "Mar": 31, "Apr": 30, "May": 31, "Jun": 30,
+                   "Jul": 31, "Aug": 31, "Sep": 30, "Oct": 31, "Nov": 30, "Dec": 31}
         if month in month_list and month != "Dec":
             next_month = month_list[month_list.index(month) + 1]
         elif month == "Dec":
@@ -101,30 +94,17 @@ def next_birth(book, month, day):  # TODO: boundary definition problems still ex
         count = 0
         print("\nThese people's birthdays within a week after", day, month)
         for i in book:
-            if month in long_month:
-                if day <= 24 and month == book[i]["month"]:
-                    if day <= book[i]["day"] <= day + 7:
-                        print(i + ":", book[i]["day"], month)
-                        count += 1
-                elif day > 24 and (month == book[i]["month"] or next_month == book[i]["month"]):
-                    if day <= book[i]["day"] <= day + 7:
-                        print(i + ":", book[i]["day"], month)
-                        count += 1
-                    elif book[i]["day"] <= 31 - day:
-                        print(i + ":", book[i]["day"], next_month)
-                        count += 1
-            elif month in short_month:
-                if day <= 23 and month == book[i]["month"]:
-                    if day <= book[i]["day"] <= day + 7:
-                        print(i + ":", book[i]["day"], month)
-                        count += 1
-                elif day > 23 and (month == book[i]["month"] or next_month == book[i]["month"]):
-                    if day <= book[i]["day"] <= day + 7:
-                        print(i + ":", book[i]["day"], month)
-                        count += 1
-                    elif book[i]["day"] <= 30 - day:
-                        print(i + ":", book[i]["day"], next_month)
-                        count += 1
+            if day <= month_x[book[i]["month"]] - 7 and month == book[i]["month"]:
+                if day <= book[i]["day"] <= day + 7:
+                    print(i + ":", book[i]["day"], month)
+                    count += 1
+            elif day > month_x[book[i]["month"]] - 7 and (month == book[i]["month"] or next_month == book[i]["month"]):
+                if day <= book[i]["day"] <= month_x[book[i]["month"]]:
+                    print(i + ":", book[i]["day"], month)
+                    count += 1
+                elif book[i]["day"] <= 7 + day - month_x[book[i]["month"]]:
+                    print(i + ":", book[i]["day"], next_month)
+                    count += 1
         if count == 0:
             print("Warning: Can't find anyone!")
     except IndexError:
@@ -215,4 +195,5 @@ def main():
 
 
 if __name__ == '__main__':
+    test_data(0)
     main()
