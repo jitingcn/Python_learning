bb31c31fe9f4f5b52c78e56b13946fd44e62490f
jiting <jitingchina@gmail.com>
2018-06-03 00:19:59 +0100

    Update(prime_number)

--- exercises/school/prime_number.py
+++ exercises/school/prime_number.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import sys
 import timeit
+from itertools import compress
 
 
 def prime(n):
@@ -18,20 +21,56 @@ def prime(n):
     return primes_l
 
 
-def main(c=3, r=1000000):
-    print("\nPrime Number: ")
+def rwh_primes1v1(n):  # https://stackoverflow.com/a/46635266
+    """ Returns  a list of primes < n for n > 2 """
+    sieve = bytearray([True]) * (n//2)
+    for i in range(3, int(n**0.5)+1, 2):
+        if sieve[i//2]:
+            sieve[i*i//2::i] = bytearray((n-i*i-1)//(2*i)+1)
+    return [2, *compress(range(3, n, 2), sieve[1:])]
+
+
+def rwh_primes1v2(n):  # https://stackoverflow.com/a/46635266
+    """ Returns a list of primes < n for n > 2 """
+    sieve = bytearray([True]) * (n//2+1)
+    for i in range(1, int(n**0.5)//2+1):
+        if sieve[i]:
+            sieve[2*i*(i+1)::2*i+1] = bytearray((n//2-2*i*(i+1))//(2*i+1)+1)
+    return [2, *compress(range(3, n, 2), sieve[1:])]
+
+
+def main(c=3, r=1000000, s=0):
+    print("\nPrime Number in %d: " % r)
     len_list = []
     start = timeit.default_timer()
     for b in range(c):
-        len_list = prime(r)
+        len_list = rwh_primes1v2(r)
     elapsed = (timeit.default_timer() - start) / c
     print("length:", len(len_list))
     print(c, "calculations average time: %.6f s" % elapsed)
-    print("Max prime number in %d: %d\n" % (r, len_list[-1]))
+    if s == 0:
+        print("Max prime: %d\n" % len_list[-1])
+    elif s == 1:
+        print(len_list)
 
 
 if __name__ == '__main__':
     try:
-        main(int(input("Enter test times: ")), int(input("Enter test range:")))
+        if os.name == "posix":
+            if len(sys.argv) < 2:
+                print("Usage: ./prime_number.py times ranges show_list(0/1)")
+                print("Example: ./prime_number.py 3 1000000")
+            else:
+                times = int(sys.argv[1])
+                ranges = int(sys.argv[2])
+                if not sys.argv[3]:
+                    show_list = 0
+                else:
+                    show_list = int(sys.argv[2])
+                main(times, ranges, show_list)
+        else:
+            main(int(input("Enter test times: ")),
+                 int(input("Enter test range:")),
+                 int(input("Show list? (0=no 1=yes): ")))
     except Exception as e:
         print(e)
