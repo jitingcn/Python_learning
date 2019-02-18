d63819192bc3dde2030b8efcbd8030b39c58fca1
Jiting <jiting@jtcat.com>
2019-02-18 23:48:39 +0800

    add PTA 1001 1002

---
+++ exercises/PTA/1002.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 1002
+# 2019/2/18 Jiting
+"""
+1002 写出这个数 （20 point(s)）
+读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
+
+输入格式：
+每个测试输入包含 1 个测试用例，即给出自然数 n 的值。这里保证 n 小于 10的100次方。
+输出格式：
+在一行内输出 n 的各位数字之和的每一位，拼音数字间有 1 空格，但一行中最后一个拼音数字后没有空格。
+
+输入样例：
+1234567890987654321123456789
+输出样例：
+yi san wu
+"""
+n = int(input())
+n_x = 0
+for i in str(n):
+    n_x += int(i)
+pinyin_list = ["ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"]
+print(" ".join((pinyin_list[int(i)] for i in str(n_x))))
